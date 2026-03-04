21063dd5e26d114c2708680164280a3bb7aa7f39
Shashank kanade <shashankkanade07@gmail.com>
2026-03-04 23:28:22 +0530

    Minimize folder structure: remove root-level duplicates, keep Krishisetu-app and Krishisetu-web

--- middleware/auth.js
+++
@@ -1,8 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-    if (req.session && req.session.userId) {
-        return next();
-    }
-    res.redirect('/login');
-};
-
-module.exports = { isAuthenticated };
